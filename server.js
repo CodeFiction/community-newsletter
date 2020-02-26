aec878f11546be128003a98757f7ffc6d47c1151
msusur <mail@mertsusur.com>
2020-02-26 00:03:21 +0000

    Adds the upload endpoint

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-require("dotenv").config();
-const express = require("express");
-const { getMessages } = require("./src/main");
+require('dotenv').config();
+const express = require('express');
+const { getMessages, uploadMessages } = require('./src/main');
 
 const app = express();
-app.set("port", process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000);
 
-app.get("/messages", async (req, res) => {
+app.get('/messages', async (req, res) => {
   return res.json(await getMessages()).status(200);
 });
 
-app.listen(app.get("port"), () =>
-  console.log(`Server is ready at http://localhost:${app.get("port")}`)
+app.get('/messages/upload', async (req, res) => {
+  const messages = await getMessages();
+  return res.json(await uploadMessages(messages)).status(200);
+});
+
+app.listen(app.get('port'), () =>
+  console.log(`Server is ready at http://localhost:${app.get('port')}`)
 );
