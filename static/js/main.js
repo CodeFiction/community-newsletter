7bdaf90ce64c010722040c261de9cb7c52b2749a
Mert Susur <msusur@gocardless.com>
2020-02-27 19:44:17 +0000

    Adds the react app

---
+++ static/js/main.js
@@ -0,0 +1,64 @@
+(function(window) {
+  window.MainBody = class MainBody extends React.Component {
+    render() {
+      return e(
+        "div",
+        { className: "main-body" },
+        e(Header, null),
+        e(MessageList, null)
+      );
+    }
+  };
+  window.Header = class Header extends React.Component {
+    render() {
+      const cfLogo = e("b", null, "CF");
+      const brand = e("div", { className: "brand" }, cfLogo, "Gündemi");
+      const main = e("div", { className: "top-bar" }, brand);
+      return main;
+    }
+  };
+  window.MessageItem = class MessageItem extends React.Component {
+    render() {
+      const reactionCounts = e(
+        "small",
+        null,
+        `[${this.props.reactionCount}] 👍 `
+      );
+
+      const linkItem = e("a", { href: this.props.link }, this.props.text);
+      return e("li", null, reactionCounts, linkItem);
+    }
+  };
+
+  window.MessageList = class MessageList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { messages: [] };
+    }
+    render() {
+      const { messages } = this.state;
+      const messageItems = [];
+
+      messages.forEach(message => {
+        messageItems.push(
+          e(MessageItem, {
+            reactionCount: message.reactionCount,
+            text: message.text,
+            link: message.links[0]
+          })
+        );
+      });
+      return e(
+        "div",
+        { className: "content" },
+        e("ol", { className: "data-list" }, ...messageItems)
+      );
+    }
+
+    componentDidMount() {
+      fetch("https://cf-community-news.herokuapp.com/messages")
+        .then(response => response.json())
+        .then(messages => this.setState({ messages }));
+    }
+  };
+})(window);
