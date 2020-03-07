2d6525249925d9f316f7a4dd5374c0e4178fd481
sonercirit <sonercirit@gmail.com>
2020-03-07 14:49:35 +0300

    implement challenge webhook initialization

--- serverless/slack/handler.js
+++ serverless/slack/handler.js
@@ -1,18 +1,15 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
+module.exports.slack = async event => {
+  const body = JSON.parse(event.body);
+  const response = {};
+
+  if (body.challenge) {
+    response.challenge = body.challenge;
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(response)
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
