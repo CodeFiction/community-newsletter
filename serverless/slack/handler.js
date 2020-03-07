cf0475c5a1d73fc8538ea47e89683732787ecd91
sonercirit <sonercirit@gmail.com>
2020-03-07 16:50:57 +0300

    adds request verification via hmac sha256

--- serverless/slack/handler.js
+++ serverless/slack/handler.js
@@ -1,6 +1,17 @@
 "use strict";
+const crypto = require("crypto");
 
 module.exports.slack = async event => {
+  console.log(event);
+
+  if (!verifyRequest(event.headers, event.body)) {
+    return {
+      statusCode: 401
+    };
+  } else {
+    console.log("request verified");
+  }
+
   const body = JSON.parse(event.body);
   const response = {};
 
@@ -13,3 +24,14 @@ module.exports.slack = async event => {
     body: JSON.stringify(response)
   };
 };
+
+function verifyRequest(headers, body) {
+  const baseString = "v0:" + headers["X-Slack-Request-Timestamp"] + ":" + body;
+
+  const hash = crypto
+    .createHmac("sha256", process.env.SIGNING_SECRET)
+    .update(baseString)
+    .digest("hex");
+
+  return "v0=" + hash === headers["X-Slack-Signature"];
+}
