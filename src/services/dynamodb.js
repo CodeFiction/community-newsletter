fd2ca55e6d4eeb9c87e80019bdfe16d379131ee8
sonercirit <sonercirit@gmail.com>
2020-03-08 17:33:08 +0300

    reformat strings

--- src/services/dynamodb.js
+++ src/services/dynamodb.js
@@ -3,11 +3,11 @@ const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
   accessKeyId: process.env.S3_ACCESS_KEY,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: 'eu-west-2'
+  region: "eu-west-2"
 });
 
 module.exports.put = async params => {
   return dynamoDB
-    .put({ TableName: 'cf-prod-messages-history', Item: params })
+    .put({ TableName: "cf-prod-messages-history", Item: params })
     .promise();
 };
