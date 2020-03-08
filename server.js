cda89638308a898a3fce419fa040b9fbece68efc
msusur <mail@mertsusur.com>
2020-03-08 14:23:41 +0000

    Adds channelId param to messages route

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ const { getMessages, uploadMessages } = require('./src/main');
 const app = express();
 app.set('port', process.env.PORT || 4000);
 
-app.get('/messages', async (req, res) => {
+app.get('/messages/:channelId?', async (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  return res.json(await getMessages()).status(200);
-});
-
-app.get('/messages/upload', async (req, res) => {
-  const messages = await getMessages();
-  return res.json(await uploadMessages(messages)).status(200);
+  const channelId = req.params.channelId || process.env.CHANNEL_ID;
+  return res.json(await getMessages(channelId)).status(200);
 });
 
 app.listen(app.get('port'), () =>
